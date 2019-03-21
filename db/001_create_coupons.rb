4f66f92d903ee06e680d0fb0a7f062c2f24e2e49
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-03-21 00:36:56 +0000

    Automatically backed up by Learn

--- db/001_create_coupons.rb
+++ db/001_create_coupons.rb
@@ -4,7 +4,7 @@ class CreateCoupons < ActiveRecord::Migration
     create_table :coupons do |t| 
       t.string :coupon_code 
       t.string :store 
-      
+      t.timestamps null: false
   end 
   
 end 
